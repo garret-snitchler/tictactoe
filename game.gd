44d8e0735fb33f773b496f2825bf9033df4f998a
Garret Snitchler <gsnitchler2@huskers.unl.edu>
2023-06-28 09:58:51 -0500

    Added game.gd file to store code.

---
+++ game.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var isXTurn
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	isXTurn = true
+
+func _onButtonRelease(x, y):
+	if isXTurn:
+		pass
+	else:
+		pass
+		
