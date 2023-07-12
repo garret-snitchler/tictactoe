2758daf69685d0795e39279680e2be32969235a7
Garret Snitchler <gsnitchler2@huskers.unl.edu>
2023-07-12 15:13:54 -0500

    Added reset button and coded a function for it to call. Added gray image asset to use for a low budget menu bar at the bottom of the screen.

--- game.gd
+++ game.gd
@@ -2,14 +2,16 @@ extends Node2D
 
 var is_x_turn
 var spaces_full
+var initial_children_count
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	is_x_turn = true
 	spaces_full = [[false, false, false], [false, false, false], [false, false, false]]
+	initial_children_count = self.get_child_count()
 
 # Function that, depending on current player, places a symbol in the location of the button pressed
-func _onButtonRelease(x, y):
+func _board_clicked(x, y):
 	if spaces_full[x][y]: # If there's already a symbol in that space nothing happens, player can click other button
 		return
 	var spr = Sprite2D.new()
@@ -23,38 +25,52 @@ func _onButtonRelease(x, y):
 	spaces_full[x][y] = true
 	is_x_turn = !is_x_turn
 
+func _reset_board(): # Resets board and game to initial state
+	spaces_full = [[false, false, false], [false, false, false], [false, false, false]]
+	var current_children_count = self.get_child_count()
+	if current_children_count > initial_children_count:
+		var i = initial_children_count
+		while i < current_children_count: # starting from the newest, remove added sprites til none are left
+			self.remove_child(self.get_child(-1))
+			i += 1
+	
+
 # Calls for each button
 func _on_button_top_left_pressed():
-	_onButtonRelease(0, 0)
+	_board_clicked(0, 0)
 
 
 func _on_button_top_center_pressed():
-	_onButtonRelease(1, 0)
+	_board_clicked(1, 0)
 
 
 func _on_button_top_right_pressed():
-	_onButtonRelease(2, 0)
+	_board_clicked(2, 0)
 
 
 func _on_button_middle_left_pressed():
-	_onButtonRelease(0, 1)
+	_board_clicked(0, 1)
 
 
 func _on_button_middle_center_pressed():
-	_onButtonRelease(1, 1)
+	_board_clicked(1, 1)
 
 
 func _on_button_middle_right_pressed():
-	_onButtonRelease(2, 1)
+	_board_clicked(2, 1)
 
 
 func _on_button_bottom_left_pressed():
-	_onButtonRelease(0, 2)
+	_board_clicked(0, 2)
 
 
 func _on_button_bottom_center_pressed():
-	_onButtonRelease(1, 2)
+	_board_clicked(1, 2)
 
 
 func _on_button_bottom_right_pressed():
-	_onButtonRelease(2, 2)
+	_board_clicked(2, 2)
+
+
+func _on_reset_button_pressed():
+	_reset_board()
