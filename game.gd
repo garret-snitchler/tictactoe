3a8f4e85d51772b59d777502ca7bf376a1694882
Garret Snitchler <gsnitchler2@huskers.unl.edu>
2023-07-26 18:08:22 -0500

    Added ItemList to keep track of player preferences for who goes first. Created foundation for code that would use this to determine the value of the is_x_turn variable.

--- game.gd
+++ game.gd
@@ -3,14 +3,19 @@ extends Node2D
 var is_x_turn
 var spaces_full
 var initial_children_count
+var starting_turn_index
+
+# https://docs.godotengine.org/en/stable/tutorials/scripting/singletons_autoload.html
+# Need to use this info to preserve current turn between scenes
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	is_x_turn = true
+	print('when is ready actually called')
+	print(is_x_turn)
 	spaces_full = [[false, false, false], [false, false, false], [false, false, false]]
 	initial_children_count = self.get_child_count()
 
-# Function that, depending on current player, places a symbol in the location of the button pressed
+# Function that, depending on current turn, places a symbol in the location of the button pressed
 func _board_clicked(x, y):
 	if spaces_full[x][y]: # If there's already a symbol in that space nothing happens, player can click other button
 		return
@@ -33,6 +38,17 @@ func _reset_board(): # Resets board and game to initial state
 		while i < current_children_count: # starting from the newest, remove added sprites til none are left
 			self.remove_child(self.get_child(-1))
 			i += 1
+	_get_starting_turn(starting_turn_index)
+	
+	
+			
+func _get_starting_turn(index):
+	if index == 0: # X first
+		print('is THIS called')
+		is_x_turn = true
+	else:
+		is_x_turn = false
+	
 	
 
 # Calls for each button
@@ -78,3 +94,8 @@ func _on_reset_button_pressed():
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://board.tscn")
+
+
+func _on_item_list_item_clicked(index, at_position, mouse_button_index):
+	starting_turn_index = index
+	_get_starting_turn(index)
