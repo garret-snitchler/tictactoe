ad41dd3c84ed43942cd669cb0ffcfc001a78dfe9
Garret Snitchler <gsnitchler2@huskers.unl.edu>
2023-07-30 15:01:28 -0500

    Added code to track when a player has won the game. Changed the way space values are tracked from booleans to numbers with specific meanings. Empty now is represented by 0, which in theory means that it would still come up as false, but all code has been changed to check for numerical values for readability's sake.

--- game.gd
+++ game.gd
@@ -1,35 +1,72 @@
 extends Node2D
 
-var spaces_full
+var space_values
 var initial_children_count
+var has_won
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_get_starting_turn(GlobalVariables.starting_turn_index)
-	spaces_full = [[false, false, false], [false, false, false], [false, false, false]]
+	space_values = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] # 0 = empty, 1 = X, 2 = O
 	initial_children_count = self.get_child_count()
-	
+	has_won = false
 
 
 # Function that, depending on current turn, places a symbol in the location of the button pressed
 func _board_clicked(x, y):
-	if spaces_full[x][y]: # If there's already a symbol in that space nothing happens, player can click other button
+	if has_won:
+		return
+	if space_values[x][y] != 0: # If there's already a symbol in that space nothing happens, player can click other button
 		return
 	var spr = Sprite2D.new()
+	var current_space_value
 	if GlobalVariables.is_x_turn:
 		spr.texture = load("res://assets/X.png")
+		current_space_value = 1
 	else:
 		spr.texture = load("res://assets/O.png")
+		current_space_value = 2
 	spr.position.x = 384*x + 128
 	spr.position.y = 384*y + 128
 	self.add_child(spr)
-	spaces_full[x][y] = true
+	space_values[x][y] = current_space_value
+	has_won = _check_victory()
 	GlobalVariables.is_x_turn = !GlobalVariables.is_x_turn
-	_adjust_turn_label()
+	if !has_won:
+		_adjust_turn_label()
+	
 
+func _check_victory(): # Check to see if the current player just won the game
+	var current_player_variable
+	if GlobalVariables.is_x_turn:
+		current_player_variable = 1
+	else:
+		current_player_variable = 2
+	var i = 0
+	while i < 3 && !has_won: # check all rows
+		if space_values[i][0] == current_player_variable && space_values[i][1] == current_player_variable && space_values[i][2] == current_player_variable:
+			has_won = true
+		i += 1
+	i = 0
+	while i < 3 && !has_won: # check all columns
+		if space_values[0][i] == current_player_variable && space_values[1][i] == current_player_variable && space_values[2][i] == current_player_variable:
+			has_won = true
+		i += 1
+	if !has_won && space_values[1][1] == current_player_variable: # checking diagonals
+		if space_values[0][0] == current_player_variable && space_values[2][2] == current_player_variable:
+			has_won = true
+		elif space_values[0][2] == current_player_variable && space_values[2][0] == current_player_variable:
+			has_won = true
+	if has_won:
+		if GlobalVariables.is_x_turn:
+			get_node("Turn Label").text = 'X Wins!'
+		else:
+			get_node("Turn Label").text = 'O Wins!'
+	return has_won
 
 func _reset_board(): # Resets board and game to initial state
-	spaces_full = [[false, false, false], [false, false, false], [false, false, false]]
+	has_won = false
+	space_values = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 	var current_children_count = self.get_child_count()
 	if current_children_count > initial_children_count:
 		var i = initial_children_count
